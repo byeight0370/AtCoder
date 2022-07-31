bbaf74a5acc63b22559ca6dda40e031b9bc2d0d1
3ao <zatton0370@gmail.com>
2022-08-01 00:59:01 +0900

    I solved 5 questions.

---
+++ Beginner_Contest/AtCoder_Beginner_Contest_240/CountDistinctIntegers.py
@@ -0,0 +1,4 @@
+N = int(input())
+a = list(map(int, input().split()))
+a = set(a)
+print(len(a))
---
+++ Beginner_Contest/AtCoder_Beginner_Contest_241/DigitMachine.py
@@ -0,0 +1,5 @@
+a = list(map(int, input().split()))
+i = 0
+for r in range(3):
+    i = a[i]
+print(i)
